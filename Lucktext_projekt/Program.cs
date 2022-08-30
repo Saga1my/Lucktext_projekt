fe4877af9348f9835bd5c4f8ea90c94b84be8ce9
Saga Palmn <Saga.palm@elev.ga.ntig.se>
2022-08-30 10:26:40 +0200

    initial commit

---
+++ Lucktext_projekt/Program.cs
@@ -0,0 +1,14 @@
+﻿string name;
+
+name = "Saga";
+
+Console.WriteLine("vad heter du?");
+
+name = Console.ReadLine();
+
+
+
+Console.WriteLine($"hej {name}");
+//Console.WriteLine(name);
+
+Console.ReadLine();
